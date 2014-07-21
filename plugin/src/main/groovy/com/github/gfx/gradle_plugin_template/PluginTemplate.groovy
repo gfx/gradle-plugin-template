f8c1c9e9b51863f3e4a8f7e561b512f337884cfe
FUJI Goro (gfx) <gfuji@cpan.org>
2014-07-21 15:59:24 +0900

    cleanup

--- plugin/src/main/groovy/com/github/gfx/gradle_plugin_template/PluginTemplate.groovy
+++ plugin/src/main/groovy/com/github/gfx/gradle_plugin_template/PluginTemplate.groovy
@@ -1,11 +1,7 @@
 package com.github.gfx.gradle_plugin_template
 
-import org.gradle.api.GradleException
 import org.gradle.api.Plugin
 import org.gradle.api.Project
-
-import java.util.zip.ZipException
-import java.util.zip.ZipFile
 
 // see http://www.gradle.org/docs/current/userguide/custom_plugins.html
 
